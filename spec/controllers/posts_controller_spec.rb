2a5a611580f1ce94e90e2256d07c9bcf0120f7a1
sagar <sagarbhandari1000@gmail.com>
2017-12-26 14:37:49 +0545

    destroy action tested

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -52,35 +52,35 @@ describe "GET #edit" do
 	end
 
 describe "PUT #update" do
-	context "when attributes are valid" do
-		it "updates the post" do
-			post = FactoryGirl.create(:post)
-			put :update, params{ id: post.id, post:FactoryGirl.attributes_for(:post, title: 'New Title', author: 'Larry')}
-			post.reload
-			expect(post.title).to eq("New Title")
-			expect(post.author).to eq("Larry")
-		end
-		it "redirects to root_path" do
-			post = FactoryGirl.create(:post)
-			put :update, params{ id: post.id, post:FactoryGirl.attributes_for(:post, title: 'New Title', author: 'Larry')}
-			expect(response).to redirect_to root_path
-		end
-	end
-	context "when attributes are invalid" do
-		it "doesn't update the post" do
-			post = FactoryGirl.create(:post) 
-			put :update, params{ id: post.id, post:FactoryGirl.attributes_for(:post, title: 'New Title', author: 'Larry', content: 'hi')}
-			post.reload
-			expect(post.title).to_not eq("New Title")
-			expect(post.author).to_not eq("Larry")
-		end
-		it "renders the edit template" do
-			post = FactoryGirl.create(:post)
-			put :update, params{ id: post.id, post:FactoryGirl.attributes_for(:post, title: 'New Title', author: 'Larry', content: 'hi')}
-			expect(esponse).to render_template :edit
-		end
-	end
-end
+    context "when attributes are valid" do
+      it "updates the post" do
+        post = FactoryGirl.create(:post)
+        put :update, params: { id: post.id, post: FactoryGirl.attributes_for(:post, title: 'New Title', author: 'Larry') }
+        post.reload
+        expect(post.title).to eq("New Title")
+        expect(post.author).to eq("Larry")
+      end
+      it "redirects to root_path" do
+        post = FactoryGirl.create(:post)
+        put :update, params: { id: post.id, post: FactoryGirl.attributes_for(:post, title: 'New Title', author: 'Larry') }
+        expect(response).to redirect_to root_path
+      end
+    end
+    context "when attributes are invalid" do
+      it "doesnot update the post" do
+        post = FactoryGirl.create(:post)
+        put :update, params: { id: post.id, post: FactoryGirl.attributes_for(:post, title: 'New Title', author: 'Larry', content: 'Hi') }
+        post.reload
+        expect(post.title).to_not eq("New Title")
+        expect(post.author).to_not eq("Larry")
+      end
+      it "renders the edit template" do
+        post = FactoryGirl.create(:post)
+        put :update, params: { id: post.id, post: FactoryGirl.attributes_for(:invalid_post) }
+        expect(response).to render_template :edit
+      end
+    end
+  end
 
 describe "GET #show" do
 	it "renders the show template" do
@@ -89,5 +89,18 @@ describe "GET #show" do
 		expect(response).to render_template :show
 		end
 	end
-
+	
+describe "DELETE #destroy" do
+    it "deletes the post" do
+      post = FactoryGirl.create(:post)
+      expect {
+        delete :destroy, params: { id: post.id }
+      }.to change(Post, :count).by(-1)
+    end
+    it "redirects to root_path" do
+      post = FactoryGirl.create(:post)
+      delete :destroy, params: { id: post.id }
+      expect(response). to redirect_to root_path
+    end
+  end
 end
