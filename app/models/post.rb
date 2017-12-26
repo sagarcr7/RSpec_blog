184b081e90974488ff77b29581f2d7790e5a8a22
sagar <sagarbhandari1000@gmail.com>
2017-12-26 10:38:00 +0545

    validations set

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+	validates :content, presence: true, length: { minimum:50 }
+	validates :title, :author, presence: true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+	it {should validate length of (:content).is_at_least(50)}
+	it {should validate_presence_of :title}
+	it {should validate_presence_of :author}
+	it {should validate_presence_of :content}
   
 end
